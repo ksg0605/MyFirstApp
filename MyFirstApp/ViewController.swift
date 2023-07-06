926269bcb56aa21f9442cdffa4e3d3d24aa19c79
ksg0605 <ksg0605@gmail.com>
2023-07-06 23:08:33 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 김선규 on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
