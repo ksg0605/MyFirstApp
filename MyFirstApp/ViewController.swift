e006916c86cb5d0d1c1ba9a019ce3bae93ebdcc9
ksg0605 <ksg0605@gmail.com>
2023-09-01 23:22:13 +0900

    finish MyFirstApp

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+        
+    @IBOutlet weak var myBotton: UIButton!
+    
+    
+    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLabel.text = "안녕하세요"
+        myBotton.backgroundColor = UIColor.yellow
+        myBotton.setTitleColor(.black, for: .normal)
+        
+//        mainLabel.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+//        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//        mainLabel.textAlignment = NSTextAlignment.center
+        
+    }
+    
+    
+    
+    
 }
 
